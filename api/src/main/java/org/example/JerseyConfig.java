b81caf7a2c586a16fc0f5820547a6c32d2608175
MarvineGothic <marvine.si@gmail.com>
2025-03-15 11:34:33 +0100

    Jetty server

--- api/src/main/java/org/example/JerseyConfig.java
+++ api/src/main/java/org/example/JerseyConfig.java
@@ -1,17 +1,20 @@
 package org.example;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import jakarta.ws.rs.ApplicationPath;
-import org.example.api.UserResource;
-import org.example.model.error.ValidationExceptionMapper;
 import org.example.model.error.ServiceExceptionMapper;
+import org.example.model.error.ValidationExceptionMapper;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
  * REST Controllers with Jersey JAX-RS
-*/
+ */
 
 @Component
 @ApplicationPath("api")
@@ -20,6 +23,9 @@ public class JerseyConfig extends ResourceConfig {
         // https://eclipse-ee4j.github.io/jersey.github.io/documentation/latest/appendix-properties.html
         property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true);
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+
+        register(RequestContextFilter.class);
+
         register(ServiceExceptionMapper.class); // register custom exception mapper
         register(ValidationExceptionMapper.class); // register validation exception mapper
         register(JacksonFeature.class);
--- service/src/main/java/org/example/database/entity/User.java
+++ service/src/main/java/org/example/database/entity/User.java
@@ -17,9 +17,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "firstName")
     private String firstName;
+
+    @Column(name = "lastName")
     private String lastName;
+
+    @Column(name = "email", unique = true)
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "role")
     private Role role;
 }
